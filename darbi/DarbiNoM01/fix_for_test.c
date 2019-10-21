996228853c78214498f17797a17bc640fed7107d
user <ghoust240@gmail.com>
2019-10-21 17:34:52 +0300

    21102019_17_34

---
+++ darbi/DarbiNoM01/fix_for_test.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+ {
+ int number1, number2, yes_no, i;
+
+ for (  yes_no=1,  i=0 ; yes_no && (i<5) ; i++)
+  {
+  printf("Cienījamais lietotāj, lūdzu, ievadi 1. skaitli: ");
+  scanf("%d",&number1);
+  printf("Cienījamais lietotāj, lūdzu, ievadi 2. skaitli: ");
+  scanf("%d",&number2);
+
+  if (number1 > number2)
+   printf("Tavs 1. skaitlis %d ir > nekā 2. skaitlis %d\n",number1,number2);
+  if (number1 < number2)
+   printf("Tavs 1. skaitlis %d ir < nekā 2. skaitlis %d\n",number1,number2);
+  //break;
+  if (i!=4)
+   {
+   printf("Cienījamais lietotāj, vai gribi turpināt (šī bija %d. reize no 5 piejamiem)?",i+1);
+   printf(" (jā - jebkurš skaitlis, izņemot 0)\n");
+   scanf("%d",&yes_no);
+   }
+  else
+   printf("Jus kluvat lidz beigam\n");
+  }
+
+ printf("Paldies par uzmanibu!\n");
+ }
