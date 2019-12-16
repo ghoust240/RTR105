dc9c4dadc63720af30cf462b04efa4f9f2ee5af3
user <ghoust240@gmail.com>
2019-12-16 16:08:40 +0200

    20191216_16_02

---
+++ darbi/series/6.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+ double x=0,y,a,S ;
+ int k=0 ;
+printf("ievetot X \n");
+scanf("%le",&x);
+ y=j0(x);
+ printf("y=sin(%.2f)=%.2f\n",x,y);
+
+ a = pow(-1,k)*pow(x,2*k+1)/(1.);
+ S = a;
+ printf("%.2f\t%8.2f\t%8.2f\n",x,a,S);
+
+ while(k<500){
+  k++;
+  a = a * (-1) *x*x / ((2*k)*(2*k+1));
+  S = S + a;
+  printf("%.2f\t%8.2f\t%8.2f\n",x,a,S);
+  }
+}
